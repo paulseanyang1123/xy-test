f4359a23f4e073e98c9107f43b9afa798bfc4cd5
xyang399 <35610008+xyang399@users.noreply.github.com>
2021-02-09 15:42:13 -0600

    shape of a flag

---
+++ flag.java
@@ -0,0 +1,22 @@
+
+public class flag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<29;i++)
+           
+{ for (int b=1;b<i;b++)
+System.out.print(" o ");//System.out.print("*"+" o "+"^");
+System.out.println(" ");
+}
+              
+for (int c=1;c<23;c++) {
+System.out.println("*");
+                       }
+		}
+	}
+// this loop is used for design a rod of flag
+
+	
+
+
