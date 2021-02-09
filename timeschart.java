a0ffc4629ee61b6615926781399c93de7a1c7f3d
xyang399 <35610008+xyang399@users.noreply.github.com>
2021-02-09 14:11:06 -0600

    9*9

---
+++ timeschart.java
@@ -0,0 +1,21 @@
+// times chart 
+
+public class timeschart
+
+{
+   public static void main(String[] args)
+
+    {  for(int x=1; x<10;x++)
+        
+            {  for(int y=1; y<10;y++)
+
+             { System.out.print(x+"*"+y+"="+(x*y));
+               //print and println difference
+               System.out.print("    ");
+              }
+         System.out.println("        ");
+           }        
+     }
+
+}
+
